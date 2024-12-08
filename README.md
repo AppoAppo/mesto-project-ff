88aa5393043376b162e5786d0444b692cacc8d27
Appo <appoappoappo@yandex.ru>
2024-12-08 23:19:39 +0300

    First try

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/AppoAppo/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,36 @@
 // @todo: Темплейт карточки
 
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".places__item");
+
 // @todo: DOM узлы
+
+const listElem = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
+function addCard(cardTitle, cardImgSrc, deleteFunction) {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImgElem = cardElement.querySelector(".card__image");
+  const cardTitleElem = cardElement.querySelector(".card__title");
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardImgElem.src = cardImgSrc;
+  cardImgElem.alt = "Альтернативный текст для изображения";
+
+  cardTitleElem.textContent = cardTitle;
+
+  cardDeleteButton.addEventListener("click", deleteFunction);
+  return cardElement;
+}
 // @todo: Функция удаления карточки
 
+const deleteCard = (evt) => evt.target.parentElement.remove();
+
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((item) => {
+  listElem.append(addCard(item.name, item.link, deleteCard));
+});
